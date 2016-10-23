81be9233005d7a83aa78f822f14b03adae7f9a61
Johnny Fee <djohnnyfee@gmail.com>
2016-10-23 10:36:30 +0800

    replace src/ and config/ with angular2class starter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  inject,
+  TestBed
+} from '@angular/core/testing';
+
+// Load the implementations that should be tested
 import { AppComponent } from './app.component';
+import { AppState } from './app.service';
+
 describe('App', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
-  });
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
+  // provide our implementations or mocks to the dependency injector
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      AppState,
+      AppComponent
+    ]}));
+
+  it('should have a url', inject([ AppComponent ], (app: AppComponent) => {
+    expect(app.url).toEqual('https://twitter.com/AngularClass');
+  }));
+
 });
