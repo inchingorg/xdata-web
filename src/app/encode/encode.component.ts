232cc76fdca5214d14d62c4cd7626b545013338f
Johnny Fee <djohnnyfee@gmail.com>
2016-12-10 02:27:58 +0800

    encode

--- src/app/encode/encode.component.ts
+++ src/app/encode/encode.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {MdSnackBarConfig, MdSnackBar} from "@angular/material";
+var xdata = require('xdata-library');
 
 @Component({
     moduleId: module.id,
@@ -8,6 +10,19 @@ import {Component} from '@angular/core';
 })
 export class EncodeComponent {
     data: string;
-    encoding: string;
-    format: string;
+    encoding: string = 'utf-8';
+    format: string = 'hex';
+    encodedResult: string = '';
+
+    constructor(public snackBar: MdSnackBar) {
+    }
+
+    encode() {
+        if (!this.data) {
+            let config = new MdSnackBarConfig();
+            this.snackBar.open('Please input the encoding data', '', config);
+            return;
+        }
+        this.encodedResult = xdata.encode(this.data, this.encoding, this.format);
+    }
 }
