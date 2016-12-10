2f8eb8d3a39810eb81bda436e0f0ccfdc3fd37c1
Johnny Fee <djohnnyfee@gmail.com>
2016-12-10 16:19:47 +0800

    encode

--- src/app/encode/encode.component.ts
+++ src/app/encode/encode.component.ts
@@ -1,28 +1,27 @@
-import {Component} from '@angular/core';
-import {MdSnackBarConfig, MdSnackBar} from "@angular/material";
-var xdata = require('xdata-library');
+import { Component} from '@angular/core';
+import { MdSnackBarConfig, MdSnackBar} from "@angular/material";
+const xdata = require('xdata-library');
 
 @Component({
-    moduleId: module.id,
-    selector: 'xd-encode',
-    templateUrl: './encode.component.html',
-    styleUrls: ['./encode.component.scss']
+  selector: 'xd-encode',
+  templateUrl: './encode.component.html',
+  styleUrls: ['./encode.component.scss']
 })
 export class EncodeComponent {
-    data: string;
-    encoding: string = 'utf-8';
-    format: string = 'hex';
-    encodedResult: string = '';
+  data: string;
+  encoding: string = 'utf-8';
+  format: string = 'hex';
+  encodedResult: string = '';
 
-    constructor(public snackBar: MdSnackBar) {
-    }
+  constructor(public snackBar: MdSnackBar) {
+  }
 
-    encode() {
-        if (!this.data) {
-            let config = new MdSnackBarConfig();
-            this.snackBar.open('Please input the encoding data', '', config);
-            return;
-        }
-        this.encodedResult = xdata.encode(this.data, this.encoding, this.format);
+  encode() {
+    if (!this.data) {
+      let config = new MdSnackBarConfig();
+      this.snackBar.open('Please input the encoding data', '', config);
+      return;
     }
+    this.encodedResult = xdata.encode(this.data, this.encoding, this.format);
+  }
 }
