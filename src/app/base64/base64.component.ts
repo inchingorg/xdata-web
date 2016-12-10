2a0d25d69a5578f7840b2280a387eeb48149a71a
Johnny Fee <djohnnyfee@gmail.com>
2016-12-10 16:19:14 +0800

    base64

--- src/app/base64/base64.component.ts
+++ src/app/base64/base64.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'xd-base64',
     templateUrl: './base64.component.html'
     // styleUrls: ['./base64.component']
