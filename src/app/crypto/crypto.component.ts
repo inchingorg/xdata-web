15811c75211e4f3ebd07b9d87895d2e5fe88305a
Johnny Fee <djohnnyfee@gmail.com>
2017-01-01 19:49:38 +0800

    crypto

--- src/app/crypto/crypto.component.ts
+++ src/app/crypto/crypto.component.ts
@@ -35,7 +35,13 @@ enum Padding {
 export class CryptoComponent {
   data: string = "00 00 00 00 00 00 00 00";
   isDataSourceHex = false;
-  isDataSourceBase64 = false;
+  isBase64: boolean = false;
+  dataSourceCount: string;
+  padedDataSource: string;
+  padedDataCount: string;
+
+  format: string = 'hex';
+  encoding: string = 'utf-8';
 
   function: string = 'TripleDES';
   iv: string = '';
@@ -48,11 +54,8 @@ export class CryptoComponent {
   padding = Padding[Padding.ZeroPadding];
   mode = Mode[Mode.ECB];
 
-  format: string = 'hex';
-  encoding: string = 'utf-8';
   result: string = '';
-  isBase64: boolean = false;
-  bytesCount: number = 0;
+  resultCount: string;
 
   modes = EnumEx.getNames(Mode);
   paddings = EnumEx.getNames(Padding);
@@ -82,12 +85,32 @@ export class CryptoComponent {
     this.isIvNeeded = true;
   }
 
+  onPadChange() {
+    let dataSource = this.formatDataSource();
+    let dataSourceWords = crypto.enc.Hex.parse(dataSource);
+    let blockSize = crypto.algo[this.function].blockSize;
+
+    crypto.pad[this.padding].pad(dataSourceWords, blockSize);
+    let padedDataSource = dataSourceWords.toString();
+    this.padedDataSource = encoder.format(padedDataSource, 'pretty');
+
+    this.padedDataCount = padedDataSource.length / 2 + ' Bytes';
+  }
+
   onDataSourceChange() {
     this.isDataSourceHex = /^[0-9A-F]+$/i.test(decoder.format(this.data));
 
     if (!this.isDataSourceHex) {
       this.isBase64 = DataUtil.isBase64String(this.data);
     }
+
+    if (this.isDataSourceHex || this.isBase64) {
+      this.dataSourceCount = decoder.format(this.data).length / 2 + ' Bytes';
+    } else {
+      this.dataSourceCount = this.data.length + ' Characters';
+    }
+
+    this.onPadChange();
   }
 
   onFunctionChange() {
@@ -135,13 +158,25 @@ export class CryptoComponent {
     return encoder.format(encodeResult, 'compressed');
   }
 
+  formatDataSource() {
+    // hex string
+    let dataSource = decoder.format(this.data);
+
+    // Plain text
+    if (!this.isDataSourceHex) {
+      dataSource = this.encode();
+    }
+
+    return dataSource;
+  }
+
   encrypt() {
     if (!this.isValid()) {
       return;
     }
 
     // hex string
-    let dataSource = decoder.format(this.data);
+    let dataSource = this.formatDataSource();
 
     // Plain text
     if (!this.isDataSourceHex) {
@@ -154,12 +189,15 @@ export class CryptoComponent {
     let key = crypto.enc.Hex.parse(decoder.format(this.key));
 
     try {
-      this.result = crypto[this.function].encrypt(encryptDataSource, key, cfg).ciphertext.toString();
+      let encryptedResult = crypto[this.function].encrypt(encryptDataSource, key, cfg).ciphertext.toString();
+      this.result = encoder.format(encryptedResult, this.format);
+      this.resultCount = encryptedResult.length / 2 + " Bytes";
+
     } catch (e) {
       this.snackBar.open(e.message, '', this.snackConfig);
     }
 
-    this.result = encoder.format(this.result, this.format);
+
   }
 
   decrypt() {
@@ -171,5 +209,6 @@ export class CryptoComponent {
 
     let result = crypto[this.function].decrypt(clipherParams, key, cfg).toString();
     this.result = encoder.format(result, this.format);
+    this.resultCount = result.length / 2 + "Bytes";
   }
 }
